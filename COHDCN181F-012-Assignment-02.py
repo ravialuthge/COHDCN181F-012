dca678a26fca94bb2041c3c9a4ae7517783d3d65
ravialuthge <39677876+ravialuthge@users.noreply.github.com>
2018-07-27 06:39:04 +0530

    Add files via upload

---
+++ COHDCN181F-012-Assignment-02.py
@@ -0,0 +1,82 @@
+import socket                                      
+import sys,os                                         
+from threading import Thread        
+                   
+                                                                         #.\COHDCN181F-012-Assignment-02.py -l 127.0.0.1 8000
+
+def server():
+	if len(sys.argv) == 4:
+          if sys.argv[1] == "-l":
+    
+            server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+            try:
+                server_socket.bind((str(sys.argv[2]),int(sys.argv[3])))
+
+            except socket.error :
+                print("invalid")
+                
+
+            server_socket.listen()
+            con , addr=server_socket.accept()
+            
+            t2=Thread(target=msg , args=(con,))
+            t2.start()
+            
+            while True:
+                try:
+                    send=input()
+                    con.send(send.encode("utf-8")) 
+                    if not send:
+                        con.send(" ".encode("utf-8"))
+            
+                    
+                except KeyboardInterrupt:
+                    con.close()
+                    sys.exit()
+	
+
+
+def client():
+	if len(sys.argv) == 3:
+   
+            client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+            try:
+                client_socket.connect((str(sys.argv[1]) ,int(sys.argv[2])))
+
+            except socket.error:
+                print("invalid")
+
+            t1=Thread(target=msg , args=(client_socket,))
+            t1.start()
+
+            while True:
+                try:
+                    send=input()
+                    client_socket.send(send.encode("utf-8"))
+                    if not send:
+                        client_socket.send(" ".encode("utf-8"))  
+                    
+
+                except KeyboardInterrupt:
+                    client_socket.close()
+                    sys.exit()
+	else:
+           print("invalid command")
+
+              
+                    
+def msg(con):     
+
+ try:
+    while True:
+        
+            data=con.recv(2048)
+            print(data.decode("utf-8"))
+ except:           
+            
+            sys.exit()
+    
+server()
+client()
